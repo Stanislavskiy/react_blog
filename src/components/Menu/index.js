18dcda1f441431d552050fdd38cbecefc0ade059
alexander <stanislavskiy.sanya@gmail.com>
2018-03-19 21:51:09 +0200

    Styles added

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,7 +6,7 @@ export default class Menu extends Component {
     return (
       <div className="Menu bg-accent d-flex justify-content-center">
         <div className="d-flex align-items-center justify-content-center ">
-          <span className="display-4 text-white">Articles</span>
+          <span className="display-3 brand-name">Articles</span>
         </div>
       </div>
     )
